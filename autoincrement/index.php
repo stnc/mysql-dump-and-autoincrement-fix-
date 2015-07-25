c1f6a5e43794e60e1028d082b6df8770fa395d3d
stnc <selmantunc@yandex.com>
2015-07-25 19:33:50 +0300

    first commit

---
+++ autoincrement/index.php
@@ -0,0 +1,70 @@
+<?php
+if (! defined ( 'DS' )) {
+	define ( 'DS', DIRECTORY_SEPARATOR );
+}
+if (! defined ( 'CRLF' )) {
+	define ( 'CRLF', PHP_EOL );
+}
+function first_array_value($value) {
+	return ($value [0] ['Field']);
+}
+function run($param=false) {
+	if ($param == 'run') {
+		include ("DatabaseInterface.php");
+		include ("Database.php");
+		
+		include ("Config.php");
+		
+		$db = new STNC\Database ();
+		
+		$q = "SHOW FULL TABLES";
+		$array_expression = $db->rows ( $q );
+		echo 'Auto increment running';
+		foreach ( $array_expression as $value ) {
+			$colonName = $value ['Tables_in_application'];
+		//	echo "\n";
+			$firstArray = first_array_value ( $db->rows ( 'SHOW COLUMNS FROM ' . $colonName ) );
+			$sql = "ALTER TABLE " . $colonName . " MODIFY COLUMN " . $firstArray . "  int(11) NOT NULL AUTO_INCREMENT FIRST;";
+			$db->querys ( $sql );
+		}
+	} else {
+		die ( "unknown params" );
+	}
+}
+
+function test1($param) {
+	echo $param.' test1';
+}
+
+function test2($param) {
+	echo $param.' test2';
+}
+
+function test3($param) {
+	echo $param .' test3';
+}
+
+
+for($i = 0; $i < count ( $argv ); $i ++) {
+
+	if ($argv [$i] == '-s') {
+		$param = $argv [$i + 1];
+		run ( $param );
+	} else if ($argv [$i] == '-d') {
+		$param = $argv [$i + 1];
+		test1 ( $param );
+	} else if ($argv [$i] == '-f') {
+		$param = $argv [$i + 1];
+		test2 ( $param );
+	} else if ($argv [$i] == '--override') {
+	
+		test3 ( 'override dan gelen  ' );
+	}
+}
+
+if ($argc < 2) {
+	$file = basename ( __FILE__ );
+	print "STNC Autoincrement Generator" . CRLF;
+	print "Usage: php -f $file -- -s komut \n [--override -d output_dir -f output_filename]" . CRLF;
+	exit ();
+}
---
+++ sql_ac.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+SET hr=%time:~0,2%
+IF %hr% lss 10 SET hr=0%hr:~1,1%
+ 
+Set TODAY=%date:~4,2%-%date:~7,2%-%date:~10,4%-%hr%%time:~3,2%%time:~6,2%%time:~9,2%
+
+ECHO.
+ 
+rem http://www.wikihow.com/Send-Sql-Queries-to-Mysql-from-the-Command-Line
+rem utf sorunu http://makandracards.com/makandra/595-dumping-and-importing-from-to-mysql-in-an-utf-8-safe-way
+rem backup icin http://webcheatsheet.com/sql/mysql_backup_restore.php
+color 2
+ECHO -----------------------mysql database delete and create database ---------------------------
+ECHO.
+"D:\EasyPHP-DevServer\binaries\mysql\bin\mysql.exe" --user=root --password= --default-character-set=utf8  application -e "DROP DATABASE application;CREATE DATABASE `application` /*!40100 COLLATE 'utf8_general_ci' */;"
+
+color 3
+ECHO ---------------------- database restore ------------------------- 
+"D:\EasyPHP-DevServer\binaries\mysql\bin\mysql.exe" --user=root --password= --default-character-set=utf8  application  < app.sql
+
+rem "D:\EasyPHP-DevServer-14.1VC11\binaries\mysql\bin\mysqldump.exe" --user=root --password=123456 --skip-opt application > diger/%TODAY%.sql"
+
+color 5
+ECHO ----------------------auto increment fix------------------------- 
+php -f autoincrement/index.php -- -s run
+
+
+color 4
+echo -------------will done happy coding-------------------------------
+PAUSE
